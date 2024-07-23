f661aba0904904b7638115c1659493068f6b4bfa
peter-sigma <petergichehacrypto@gmail.com>
2024-07-23 13:36:43 +0000

    1. Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+   Simple pagination sample.
+"""
+import csv
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+       Retrieve index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """
+      Server class to paginate the database Popular_Baby_Names.csv.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """
+           Initializes the new server instance.
+        """
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+           Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+           Retrieves a page with data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]

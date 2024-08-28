abf7d42922fb938d78a2b7dce817f9769f3b3ee0
peter-sigma <petergichehacrypto@gmail.com>
2024-08-28 19:15:50 +0000

    9. Track progress and errors with Kue: Create the Job processor

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,7 +1,7 @@
 import { createQueue } from 'kue';
 
 const queue = createQueue();
-const jobData = { phoneNumber: '+25497711372', message: 'Your order is on its way. Thank you for shopping with us.' };
+const jobData = { phoneNumber: '+25497711372', message: 'MESSAGE: Received notification from kue->queue.create' };
 const job = queue.create('push_notification_code', jobData).save((error) => {
   if (!error) console.log(`Notification job created: ${job.id}`);
 });
